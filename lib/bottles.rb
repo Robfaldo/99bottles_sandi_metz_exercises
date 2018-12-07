9ef587170b2d7083a7f163b5fef9c4cd8a82c073
Robert Faldo <robert.faldo@simplybusiness.co.uk>
2018-12-07 16:03:38 +0000

    complete test_another_verse

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,7 +1,7 @@
 class Bottles
   def verse(bottles)
     <<~VERSE
-      #{bottles} bottles of beer on the wall, #{bottles} bottles of beer. 
+      #{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
       Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.
     VERSE
   end
--- test/bottles_test.rb
+++ test/bottles_test.rb
@@ -5,14 +5,13 @@ require_relative '../lib/bottles'
 class BottlesTest < Minitest::Test
   def test_the_first_verse
     expected = <<-VERSE
-99 bottles of beer on the wall, 99 bottles of beer. 
+99 bottles of beer on the wall, 99 bottles of beer.
 Take one down and pass it around, 98 bottles of beer on the wall.
 VERSE
     assert_equal expected, ::Bottles.new.verse(99)
   end
 
   def test_another_verse
-    skip
     expected = <<-VERSE
 89 bottles of beer on the wall, 89 bottles of beer.
 Take one down and pass it around, 88 bottles of beer on the wall.
