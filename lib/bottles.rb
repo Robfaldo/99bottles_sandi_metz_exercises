2938fa63445b95275629337e50644212e5370404
Robert Faldo <robert.faldo@simplybusiness.co.uk>
2018-12-07 16:14:14 +0000

    complete test_verse_2

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,8 +1,8 @@
 class Bottles
-  def verse(bottles)
+  def verse(num_of_bottles)
     <<~VERSE
-      #{bottles} bottles of beer on the wall, #{bottles} bottles of beer.
-      Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.
+      #{num_of_bottles} bottles of beer on the wall, #{num_of_bottles} bottles of beer.
+      Take one down and pass it around, #{num_of_bottles - 1} #{num_of_bottles - 1 == 1 ? 'bottle' : 'bottles' } of beer on the wall.
     VERSE
   end
 end
